881a5e300b64725b7be6b9dd9cf0aa182f2d64f4
Elena Ziani <zianielena@gmail.com>
2023-10-27 09:48:54 +0200

    components added

---
+++ src/app/coins-modal/coins-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coins-modal',
+  templateUrl: './coins-modal.component.html',
+  styleUrls: ['./coins-modal.component.css']
+})
+export class CoinsModalComponent {
+
+}
